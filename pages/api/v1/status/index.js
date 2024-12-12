3392379ac0482c083af4367dd1b429499b3ad541
Samuel Pereira <gmr.quest.10@gmail.com>
2024-12-11 23:48:27 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result.rows);
+
   response.status(200).json({ message: "API em funcionamento!" });
 };
 
