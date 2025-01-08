b13cc68432a245f835c0d4945a8a4245d3e48871
Samuel Pereira <gmr.quest.10@gmail.com>
2025-01-07 21:55:20 -0300

    add services scripts

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,6 @@ import database from "infra/database.js";
 const status = async (request, response) => {
   const result = await database.query("SELECT 1 + 1 as sum;");
 
-  console.log(result.rows);
-
   response.status(200).json({ message: "API em funcionamento!" });
 };
 
