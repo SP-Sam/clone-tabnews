15d22535d5807844c0ca0b479ca373b2d23f4e32
Samuel Pereira <gmr.quest.10@gmail.com>
2025-01-09 00:27:18 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updateAt = new Date().toISOString();
 
-  response.status(200).json({ message: "API em funcionamento!" });
+  const dbVersionQuery = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionQuery.rows[0].server_version;
+
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsQuery.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbOpenedConnectionsQuery = await database.query({
+    text: `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],
+  });
+
+  const dbOpenedConnections =
+    dbOpenedConnectionsQuery.rows[0].opened_connections;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+      },
+    },
+  });
 };
 
 export default status;
