9526d6344667c50787482368754baecd0bf12b2a
Samuel Pereira <gmr.quest.10@gmail.com>
2025-01-09 22:42:55 -0300

    fix certificate config

--- infra/database.js
+++ infra/database.js
@@ -12,7 +12,7 @@ const query = async (queryObject) => {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "production" && {
-      rejectUnauthorized: true,
+      rejectUnauthorized: false,
       ca: caCertificate,
     },
   });
