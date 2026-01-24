5d06e7b2507c1f60d62a9f5f9e95806ae7ecc2ad
Samuel Pereira <gmr.quest.10@gmail.com>
2026-01-24 00:14:26 -0300

    creates `/migrations` endpoint

---
+++ pages/api/v1/migrations/index.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import migrationRunner, { RunnerOption } from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+async function migrations(request: NextApiRequest, response: NextApiResponse) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions: RunnerOption = {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: true,
+    });
+
+    dbClient.end();
+
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const executedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    dbClient.end();
+
+    if (executedMigrations.length > 0) {
+      return response.status(201).json(executedMigrations);
+    }
+
+    return response.status(200).json(executedMigrations);
+  }
+
+  return response.status(405).end();
+}
+
+export default migrations;
