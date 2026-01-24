062606ba1edcfbd8717116750459bd543d278cf6
Samuel Pereira <gmr.quest.10@gmail.com>
2026-01-24 00:11:05 -0300

    improves env vars usage to test environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.development" });
+
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
 };
