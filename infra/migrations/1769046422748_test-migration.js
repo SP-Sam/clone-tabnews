b2b91287b83ff67a2192730ef95bec75be064547
Samuel Pereira <gmr.quest.10@gmail.com>
2026-01-25 17:22:28 -0300

    revert modifications

--- infra/migrations/1769046422748_test-migration.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-// import type { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
-
-export const shorthands = undefined;
-
-export async function up(pgm) {}
-
-export async function down(pgm) {}
