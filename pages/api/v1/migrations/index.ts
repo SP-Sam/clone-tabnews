201837415a000fab4538752d597c6d507e640c2f
Samuel Pereira <gmr.quest.10@gmail.com>
2026-01-25 16:29:10 -0300

    Revert "fix: forces dynamic imports for migrationRunner"
    This reverts commit 2667e537dd01d89d1e4ead0450a9583ea4c05f20.

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -6,13 +6,12 @@ import database from "infra/database";
 async function migrations(request: NextApiRequest, response: NextApiResponse) {
   const dbClient = await database.getNewClient();
 
-  const defaultMigrationOptions = {
+  const defaultMigrationOptions: RunnerOption = {
     dbClient: dbClient,
     dir: join("infra", "migrations"),
     direction: "up",
     migrationsTable: "pgmigrations",
-    loader: (filePath: string) => import(filePath),
-  } as RunnerOption;
+  };
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner({
