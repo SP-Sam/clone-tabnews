53007daec4ae49b338486075350af10d1a05a6f0
Samuel Pereira <gmr.quest.10@gmail.com>
2026-01-28 19:15:49 -0300

    remove fix

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -39,7 +39,7 @@ async function migrations(request: NextApiRequest, response: NextApiResponse) {
     return response.status(200).json(executedMigrations);
   }
 
-  dbClient.end();
+  // dbClient.end();
 
   return response.status(405).end();
 }
