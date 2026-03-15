d6bd37b7a4365f5beff5f8806bba808adbcc68e3
Samuel Pereira <gmr.quest.10@gmail.com>
2026-03-14 22:55:34 -0300

    feat: create `/status` page

---
+++ pages/status/index.tsx
@@ -0,0 +1,63 @@
+import useSWR from "swr";
+
+const LOADING_TEXT = "Carregando...";
+
+async function fetchAPI(key: string) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+
+  return responseBody;
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+    fallbackData: LOADING_TEXT,
+  });
+
+  let updatedAtText = data;
+
+  if (!isLoading && data !== LOADING_TEXT) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <h2>Última atualização: {updatedAtText}</h2>;
+}
+
+function DataBaseInfos() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+    fallbackData: LOADING_TEXT,
+  });
+
+  let maxConnections = data;
+  let openedConnections = data;
+  let databaseVersion = data;
+
+  if (!isLoading && data !== LOADING_TEXT) {
+    maxConnections = data.dependencies.database.max_connections;
+    openedConnections = data.dependencies.database.opened_connections;
+    databaseVersion = data.dependencies.database.version;
+  }
+
+  return (
+    <div>
+      <h2>Informações do banco de dados</h2>
+      <p>Máximo de conexões aceitas: {maxConnections}</p>
+      <p>Conexões abertas: {openedConnections}</p>
+      <p>Versão do banco: {databaseVersion}</p>
+    </div>
+  );
+}
+
+function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DataBaseInfos />
+    </>
+  );
+}
+
+export default StatusPage;
