d30c8eb059fc6e633cec7c708c39e7360cc898f1
Samuel Pereira <gmr.quest.10@gmail.com>
2026-03-28 22:44:22 -0300

    feat: create and use `InternalServerError`

---
+++ infra/errors.ts
@@ -0,0 +1,21 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({ cause }) {
+    super("Houve um erro interno inesperado", { cause });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
