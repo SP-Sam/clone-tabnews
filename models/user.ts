cdd1bc7df4e89317af5cdaae2e876aa1690117e1
Samuel Pereira <gmr.quest.10@gmail.com>
2026-04-12 14:31:35 -0300

    feat: implement user creation with unique email and username validation

---
+++ models/user.ts
@@ -0,0 +1,78 @@
+import database from "infra/database";
+import { ValidationError } from "infra/errors";
+
+interface CreateUserDTO {
+  username: string;
+  email: string;
+  password: string;
+}
+
+async function create(userInputValues: CreateUserDTO) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function validateUniqueEmail(email: string) {
+    const result = await database.query({
+      text: `
+        SELECT
+          email
+        FROM
+          users
+        WHERE
+          LOWER(email) = LOWER($1)
+        ;`,
+      values: [email],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username: string) {
+    const result = await database.query({
+      text: `
+        SELECT
+          username
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        ;`,
+      values: [username],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError({
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValues: CreateUserDTO) {
+    const result = await database.query({
+      text: `
+      INSERT INTO
+        users (username, email, password)
+      VALUES
+        ($1, $2, $3)
+      RETURNING
+        *
+      ;`,
+      values: [userInputValues.username, userInputValues.email, userInputValues.password],
+    });
+
+    return result.rows[0];
+  }
+}
+
+export default {
+  create,
+};
